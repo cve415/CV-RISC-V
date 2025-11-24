c6c54b13e8e55878bfa8a0b48d65826a8fa39b4e
Christopher Velasco <51070269+cve415@users.noreply.github.com>
2025-11-23 16:46:49 -0800

    Implement basic Jobs page layout

--- app/jobs/page.tsx
+++ app/jobs/page.tsx
@@ -1,1 +1,11 @@
-Jobs page, TBA...
+import React from 'react';
+import { Text } from '@radix-ui/themes';
+
+export default function JobsPage() {
+  return (
+    <div>
+      <h1>Careers</h1>
+      <Text>Stay tuned!</Text>
+    </div>
+  );
+}
